ea3ccc7b4825d376a2bfafbca0ec312e5ce62a44
xdevs23 <xdevs23@outlook.com>
2016-08-13 19:07:55 +0200

    CNativeIncludes: introduce prntutls.h

--- CNativeIncludes/prntutls.h
+++ CNativeIncludes/prntutls.h
@@ -6,5 +6,10 @@ static void printn(char *msg) {
     printf("%s\n", msg);
 }
 
+static int8_t nomemory() {
+    printn("Error: Memory not allocatable or no free memory available");
+    return 1;
+}
+
 
 #endif // _PRNTUTLS_H_
