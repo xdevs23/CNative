de85a9b0625dc17e83cc9632b9f9b0eff93ca686
xdevs23 <xdevs23@outlook.com>
2016-08-13 19:01:37 +0200

    CNative:main: check for memory allocation

--- CNative/main.c
+++ CNative/main.c
@@ -5,20 +5,32 @@
 #include "prntutls.h"   // Print utils
 #include "cli/cli.h"    // Command Line Interpreter
 
+#define MAIN_CMD_MAX_SIZE 2048
+
 /**
  * Main entry point
  */
 int main() {
     cnative_print_welcome_message();
 
-    char *cmd = malloc(sizeof(cmd)); // Allocate memory for cmd, avoid segfault
+    // Allocate memory for cmd, avoid segfault
+    char *cmd = malloc(MAIN_CMD_MAX_SIZE);
+
+    // Check for memory
+    if(cmd == NULL) goto nomemory;
+
     // Loop if not exit or quit
     while(strcasecmp(cmd, "exit") && strcasecmp(cmd, "quit")) {
         printf(" > ");
         char *tempcmd = malloc(sizeof(tempcmd));
+        if(tempcmd == NULL) goto nomemory
         scanf("%s", tempcmd);
         strcpy(cmd, tempcmd);
         cli_handle_command(cmd);
     }
     return 0;
+
+nomemory:
+    printn("Error: Memory not allocatable or no free memory available");
+    return 1;
 }
