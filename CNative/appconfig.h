455e3115fb582de5f747c29ea67facd203748eba
xdevs23 <xdevs23@outlook.com>
2016-08-13 17:04:00 +0200

    CNative: add appconfig

---
+++ CNative/appconfig.h
@@ -0,0 +1,13 @@
+#ifndef _APPCONFIG_H_
+#define _APPCONFIG_H_
+
+static struct cnative_version_info {
+    int16_t major;
+    int16_t minor;
+    int16_t rev;
+};
+
+struct cnative_version_info cnative_version = { 1, 0, 0 };
+
+
+#endif // _APPCONFIG_H_
