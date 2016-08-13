7c0c9d7acedd05618002b0d2b5b7722f14bf2509
xdevs23 <xdevs23@outlook.com>
2016-08-13 20:26:06 +0200

    CNativeIncludes:strutls: add string_checksize

--- CNativeIncludes/strutls.h
+++ CNativeIncludes/strutls.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "errs.h"
 
 static char* string_tolower(char *str) {
     for (int i = 0; str[i]; i++)
@@ -15,6 +16,10 @@ static char* string_checkinput_newline(char *input) {
     if ((strlen(input) > 0) && (input[strlen(input) - 1] == '\n'))
         input[strlen(input) - 1] = '\0';
     return input;
+}
+
+static int string_checksize(char *src, char *cmp) {
+    return strlen(src) >= cmp;
 }
 
 #endif // _STRUTLS_H_
